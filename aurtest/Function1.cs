4956453533969ddf7597ff1cefbda728a53f859c
Urban <Urban@URBAN-DESKTOP>
2022-05-28 03:48:14 +0200

    Working

--- aurtest/Function1.cs
+++ aurtest/Function1.cs
@@ -7,30 +7,94 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using Discord;
+using Discord.WebSocket;
+using Discord.Rest;
+using Discord.Net;
 
 namespace aurtest
 {
     //change 2
-    public static class Function1
+    public class Function1
     {
+        private const string PublicKey = "8f32935a699deed492cdf6610888babebbbaef5c9c20930fea2ca3b24df331db";
+        private readonly DiscordRestClient client;
+
+        public Function1(DiscordRestClient client)
+        {
+            this.client = client;
+        }
+
         [FunctionName("Function4")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-            return new OkObjectResult(responseMessage);
+            string timestamp = req.Headers["X-Signature-Timestamp"];
+            string signature = req.Headers["X-Signature-Ed25519"];
+            //string body = await req.ReadAsStringAsync();
+
+            MemoryStream ms = new MemoryStream();
+            req.Body.CopyTo(ms);
+            byte[] bytes = ms.ToArray();
+
+            RestInteraction restInteraction = await client.ParseHttpInteractionAsync(PublicKey, signature, timestamp, bytes);
+            try
+            {
+                if (restInteraction.Type == InteractionType.Ping)
+                {
+                    string v = ((RestPingInteraction)restInteraction).AcknowledgePing();
+                    return new ContentResult() {Content = v, StatusCode = 200, ContentType = "application/json" };
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+
+            if (restInteraction.IsDMInteraction)
+            {
+                await restInteraction.User.SendMessageAsync("gay");
+            }
+            else
+            {
+                
+                await restInteraction.Channel.SendMessageAsync("gay");
+            }
+            string v1 = restInteraction.Respond("extra gay");
+
+            return new ContentResult() { Content = v1, StatusCode = 200, ContentType = "application/json" };
+        }
+
+        [FunctionName("Function5")]
+        public async Task<IActionResult> Run2(
+       [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+       ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            await Client_Ready();
+
+
+            return new OkResult();
+        }
+
+        public async Task Client_Ready()
+        {
+            RestGuild restGuild = await client.GetGuildAsync(538880298830790686);
+
+            var guildCommand = new SlashCommandBuilder();
+            guildCommand.WithName("bad");
+            guildCommand.WithDescription("Your bad");
+
+            await restGuild.CreateApplicationCommandAsync(guildCommand.Build());
         }
     }
 }
---
+++ aurtest/startup.cs
@@ -0,0 +1,26 @@
+﻿using aurtest.Startup;
+using Discord.Rest;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(Bootstrapper))]
+
+namespace aurtest.Startup
+{
+    internal class Bootstrapper : FunctionsStartup
+    {
+        private static readonly string ApplicationGoogleDriveServiceAccountId = Environment.GetEnvironmentVariable(nameof(ApplicationGoogleDriveServiceAccountId), EnvironmentVariableTarget.Process);
+        private static readonly string ApplicationGoogleDriveServiceAccountPrivateKey = Environment.GetEnvironmentVariable(nameof(ApplicationGoogleDriveServiceAccountPrivateKey), EnvironmentVariableTarget.Process);
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services
+               .AddSingleton<DiscordRestClient>(c => { 
+                   DiscordRestClient discordRestClient = new();
+                   discordRestClient.LoginAsync(Discord.TokenType.Bot, "OTc5ODQ2NDIwNDU2NjczMzYx.GF3WDT.OYXqupAV7crpU9U4I72ONSLrYVQav9O0Loyo4k").Wait();
+                   return discordRestClient; 
+               });
+        }
+    }
+}
